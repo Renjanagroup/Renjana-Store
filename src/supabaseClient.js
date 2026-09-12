d115884baa1dc1bd2cbd5efd3b8986f0c705fa5a
Renjana <bongkysetiawan079@gmail.com>
2026-09-12 12:15:49 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = https://pwpobnxmodafvbdiybul.supabase.co/rest/v1/
+const supabaseAnonKey = sb_publishable_ozrotJNcT-xWGEEb9YAGAA_5Z3tXFgD
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn(
