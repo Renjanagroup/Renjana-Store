0c3f2c8e714f7743beefa5c403bb3eeb74bb1edd
Renjana <bongkysetiawan079@gmail.com>
2026-09-12 12:30:32 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // WAJIB sama persis dengan nama repo GitHub kamu (huruf besar/kecil harus cocok)
-  base: '/Renjana-Store/',
-})
